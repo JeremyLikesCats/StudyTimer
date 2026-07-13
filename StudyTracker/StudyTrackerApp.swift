0f3be128710f6897231a6460b36d2debe1c2b7da
Jeremy Zhou <thejeremyzhou@gmail.com>
2026-07-13 14:12:02 +0900

    Initial Commit

---
+++ StudyTracker/StudyTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  StudyTrackerApp.swift
+//  StudyTracker
+//
+//  Created by Jeremy Zhou on 13/7/2026.
+//
+
+import SwiftUI
+
+@main
+struct StudyTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
